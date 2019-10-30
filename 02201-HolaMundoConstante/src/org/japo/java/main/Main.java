91a98bb3aa2c993edc0ed95c824796ba694bd3f9
malabo1990 <malabo1990@gmail.com>
2019-10-30 23:02:35 +0100

    Add files via upload

---
+++ 02201-HolaMundoConstante/src/org/japo/java/main/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Casimiro Ondo Obiang casimiro.ondo.alum@iescamp.es
+ */
+public class Main {
+    public static void main(String[] args) {
+    
+        //Ejercicio: 02201-TallaZapatoConsola
+        final String SALUDO_CONSTANTE = "¡Hola mundo!";
+        System.out.println(SALUDO_CONSTANTE);
+    }
+}
