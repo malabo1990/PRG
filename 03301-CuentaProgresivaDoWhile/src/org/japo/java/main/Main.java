ce5bbd9bcb0b57343d2f54ccccfd028a0b74f033
malabo1990 <malabo1990@gmail.com>
2019-11-03 23:27:55 +0100

    Add files via upload

---
+++ 03301-CuentaProgresivaDoWhile/src/org/japo/java/main/Main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.japo.java.main;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Casimiro Ondo Obiang
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //EJERCICIO: 03301 EXAMENES DE RESPUESTAS
+        //Constante
+        final int INI= 0;
+        final int MAX = 9;
+        
+//variable
+        int i = INI;
+        
+        //mensaje
+        do{
+       System.out.println(i);
+       i++;}
+        
+       while (i<MAX);
+    }
+}
+    
+
---
+++ 03302-CuentaProgresivaWhile/src/org/japo/java/main/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.japo.java.main;
+
+/**
+ *
+ * @author Casimiro Ondo Obiang casimiro.ondo.alum@iescamp.es
+ */
+public class Main {
+    public static void main(String[] args) {
+        //EJERCICIO: 03302 Cuenta Progresiva While
+      //CONSTANTE
+      final int MAX = 9;
+      final int MIN = 0;
+      
+      //variable
+      int i=MIN;
+      
+      
+      //bucle con while
+      while (i<MAX){
+      
+      System.out.println(i);
+      i++;
+      }
+      
+    }
+    
+    
+}
